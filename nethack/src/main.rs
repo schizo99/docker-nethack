6e78ee0c080afcc1c91a09161054160dab259ddd
Andreas Åman <andreas.aman@atg.se>
2024-08-05 14:10:26 +0200

    Switch to using console crate and listening to single keypress

--- nethack/src/main.rs
+++ nethack/src/main.rs
@@ -1,13 +1,15 @@
+use console::Term;
 fn main() {
+    let term = Term::stdout();
     let output = std::process::Command::new("/nh367/nethack")
         .arg("-s")
         .output()
         .expect("failed to execute process");
-    print!("\x1B[2J\x1B[1;1H");
-    println!("{}", String::from_utf8_lossy(&output.stdout));
-    println!("<More>");
-    print!("\x1B[?25l");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    print!("\x1B[?25h");
+    let result = std::str::from_utf8(&output.stdout).expect("Failed to convert output to string");
+    term.clear_screen().expect("Failed to clear terminal");
+    term.hide_cursor().expect("Failed to hide cursor");
+    term.write_line(result).expect("Failed to write to terminal");
+    term.write_line("<More>").expect("Failed to write to terminal");
+    term.read_key().expect("Failed to read key");
+    term.show_cursor().expect("Failed to show cursor");
 }
